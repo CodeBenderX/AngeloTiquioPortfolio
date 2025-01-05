1c3a63121c65510d15f23d7762fbb937b8299da2
05dffc1 <angelo.tiquio@outlook.com>
2025-01-04 22:25:44 -0500

    modified MainRouter, style, and added Project component

--- client/Component/MainRouter.jsx
+++ client/Component/MainRouter.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Component/Home.jsx";
-// import About from "./components/About";
+import About from "../Component/About.jsx";
 // import Contact from "./components/Contact";
 // import Services from "./components/Services";
 import Layout from "../Component/Layout.jsx";
-// import Project from "./components/Project";
+import Project from "../Component/Project.jsx";
 import Footer from "../Component/Footer.jsx";
 
 const MainRouter = () => {
@@ -14,8 +14,9 @@ const MainRouter = () => {
       <Layout />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        {/* <Route exact path="/about" element={<About />} />
+        <Route exact path="/about" element={<About />} />
         <Route exact path="/project" element={<Project />} />
+        {/* 
         <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/services" element={<Services />} /> */}
       </Routes>
